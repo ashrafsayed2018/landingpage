29470cfe3d43e33547d15115e7e13213f858b72d
nql-afesh <monaalii35094@gmail.com>
2024-09-21 06:45:04 +0300

    adding articles page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to left, #002730, #004b54)',
+      },
+      colors: {
+        'light-blue': '#004b54', // Lighter shade for hover
+        'lighter-blue': '#006c73', // Even lighter shade for background
+      },
+    },
   },
   plugins: [],
 }
